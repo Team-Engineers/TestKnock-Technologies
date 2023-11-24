e9e9c03854f28f95623ec2eb96cc5048a9d4c6bc
ditya17 <nitya17nitya@gmail.com>
2023-11-25 04:41:47 +0530

    updated-team-and-home-page

--- src/Components/Banner/BannerHome.jsx
+++ src/Components/Banner/BannerHome.jsx
@@ -5,9 +5,9 @@ const BannerHome = () => {
     <div className="bannerHome text-white md:h-[45vw] h-[150vw] bg-[#e7d987]">
       <div className="max-w-[800px] relative top-[50%] md:top-[40%] mx-auto flex flex-col justify-center items-center">
         <div className="mt-[-96px] text-center">
-          <p className="text-[white] font-bold p-2 md:text-5xl text-2xl ">Revise Smarter, Score Better:</p>
-          <p className="text-[#c9c0c0] font-bold p-1 md:text-1.5xl text-1xl">with our</p>
-          <h1 className="text-[white] md:text-6xl sm:text-4xl text-2xl font-bold md:p-2 w-[70vw]">
+          <p className="page_font text-[white] font-bold p-2 md:text-5xl text-2xl ">Revise Smarter, Score Better:</p>
+          <p className="page_font text-[#c9c0c0] font-bold p-1 md:text-1.5xl text-1xl">with our</p>
+          <h1 className="page_font  text-[white] md:text-6xl sm:text-4xl text-2xl font-bold md:p-2 w-[70vw]">
           Expert Solutions, Scorecards, and Top-tier Technical & IT Services
           </h1>
         </div>  
