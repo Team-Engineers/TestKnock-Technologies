d9512115948f48bed4b3db86be7d26ef2a71cb47
Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>
2023-11-29 07:56:50 +0530

    remove carousel from ouradvisory.jsx

--- src/Components/About/OurAdvisory.jsx
+++ src/Components/About/OurAdvisory.jsx
@@ -1,96 +1,139 @@
-import React, { useRef, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import "./ouradvisory.css";
-import { Navigation, Autoplay, Pagination, FreeMode } from 'swiper/modules';
-
-const advisoryData = [
-    {
-        id: 1,
-        imageUrl: require('../../Assets/Images/advisory/animesh_ikshit.jpeg'),
-        name: 'Animesh Ikshit',
-        qualification: 'B. Des. from NIFT Delhi M.A. in DESIGN STUDIES from NOTTINGHAM TRENT UNIVERSITY (U.K.)',
-        expertise: 'DESIGN AND CREATIVITY EXPERT CORE MEMBER',
-    },
-    {
-        id: 2,
-        imageUrl: require('../../Assets/Images/advisory/manoj_jha.jpeg'),
-        name: 'Manoj Jha',
-        qualification: 'Management Graduate',
-        expertise: 'Expert in STRATEGIES',
-    },
-    {
-        id: 3,
-        imageUrl: require('../../Assets/Images/advisory/prasanjit_sharma.jpeg'),
-        name: 'Prasanjit Sharma',
-        qualification: 'B. Tech from IIT Delhi',
-        expertise: 'Tech Architech',
-    },
-    {
-        id: 4,
-        imageUrl: require('../../Assets/Images/advisory/omansh_mathur.jpeg'),
-        name: 'Omansh Mathur',
-        qualification: 'Graduated with a Dual Degree focused in Mathematics and Computing from Indian Institute of Technology (Banaras Hindu University), Varanas',
-        expertise: 'SDE 3 GOOGLE',
-    },
-    {
-        id: 5,
-        imageUrl: require('../../Assets/Images/advisory/pranav_vijayvaran.jpeg'),
-        name: 'pranav vijayvaran',
-        qualification: 'IIT Bombay',
-        expertise: 'Senior Software Engineer Microsoft',
-    },
-];
+import React from "react";
 
 const OurAdvisory = () => {
-    return (
-        <>
-            <div className="flex justify-center ">
-                <h2 className="font-bold text-[200%] mb-[50px] ">OUR ADVISORY TEAM</h2>
+  return (
+    <>
+      <div className="flex justify-center ">
+        <h2 className="heading_main text-center font-bold md:my-[2%] my-[12%] md:text-[40px] text-[30px] ">OUR ADVISORY TEAM</h2>
+      </div>
+      <div className="h-[120%]">
+        <div className="grid mr-auto ml-auto justify-center grid-cols-1  md:grid-cols-3 ">
+          <div className="flex  flex-col">
+            <div className="w-50 h-50 rounded-full mr-auto ml-auto flex justify-center">
+              <img
+                className="rounded-full h-[180px] w-[180px] my-[20px]"
+                src={require("../../Assets/Images/advisory/animesh_ikshit.jpeg")}
+                alt=""
+              />
             </div>
-            <div className='h-[120%] w-[80%] swiperrr'>
-                <Swiper
-                    style={{
-                        '--swiper-navigation-color': 'black',
-                        '--swiper-pagination-color': 'blue',
-                    }}
-                    cssMode={true}
-                    spaceBetween={30}
-                    centeredSlides={true}
-                    autoplay={{
-                        delay: 2000,
-                        disableOnInteraction: false,
-                    }}
-                    loop={true}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    navigation={true}
-                    modules={[Autoplay, FreeMode, Pagination, Navigation]}
-                    className="mySwiper"
-                >
-                    {advisoryData.map((advisor) => (
-                        <SwiperSlide key={advisor.id}>
-                            <div className='flex flex-row '>
-                                <div>
-                                    <img src={advisor.imageUrl} alt="" />
-                                </div>
-                                <div className='w-[80%] mr-[20px] ml-[20px] mt-auto mb-auto'>
-                                    <h1>
-                                        <span className='text-[150%] font-bold'>{advisor.name}</span><br />
-                                        {advisor.qualification}<br />
-                                        {advisor.expertise}
-                                    </h1>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
+            <div className="w-[300px] mr-auto ml-auto  mt-auto mb-auto">
+              <h1 className="mr-auto  ml-auto text-center">
+                <span className="text-[200%]  font-bold">Animesh Ikshit</span>
+                <br />
+                <span className="yellowww text-[#063970] font-bold  text-[80%]">
+                  DESIGN AND CREATIVITY EXPERT CORE MEMBER
+                </span>
+                <br />
+                <span className="text-[90%] font-style: italic">
+                  B. Des. from NIFT Delhi M.A. in DESIGN STUDIES from NOTTINGHAM
+                  TRENT UNIVERSITY (U.K.)
+                </span>
+                <br />
+              </h1>
             </div>
-        </>
-    );
-}
+          </div>
+          <div className="flex  flex-col ">
+            <div className="w-50 h-50 rounded-full mr-auto ml-auto flex justify-center">
+              <img
+                className="rounded-full h-[180px] w-[180px] my-[20px]"
+                src={require("../../Assets/Images/advisory/manoj_jha.jpeg")}
+                alt=""
+              />
+            </div>
+            <div className="w-[300px] mr-auto ml-auto  mt-auto mb-auto">
+              <h1 className="mr-auto  ml-auto text-center">
+                <span className="text-[200%]  font-bold">Manoj Jha</span>
+                <br />
+                <span className="yellowww text-[#063970] font-bold  text-[80%]">
+                  EXPERT IN STRATEGIES
+                </span>
+                <br />
+                <span className="text-[90%] font-style: italic">
+                  Management Graduate
+                </span>
+                <br />
+              </h1>
+            </div>
+          </div>
+          <div className="flex  flex-col ">
+            <div className="w-50 h-50 rounded-full mr-auto ml-auto flex justify-center">
+              <img
+                className="rounded-full h-[180px] w-[180px] my-[20px]"
+                src={require("../../Assets/Images/advisory/prasanjit_sharma.jpeg")}
+                alt=""
+              />
+            </div>
+            <div className="w-[300px] mr-auto ml-auto  mt-auto mb-auto">
+              <h1 className="mr-auto  ml-auto text-center">
+                <span className="text-[200%]  font-bold">Prasanjit Sharma</span>
+                <br />
+                <span className="yellowww text-[#063970] font-bold  text-[80%]">
+                  TECH ARCHITECT
+                </span>
+                <br />
+                <span className="text-[90%] font-style: italic">
+                  B. Tech from IIT Delhi
+                </span>
+                <br />
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="flex md:flex-row flex-col  justify-center  mr-auto ml-auto my-[5%] ">
+          <div className="flex  flex-col ">
+            <div className="w-50 h-50 rounded-full mr-auto ml-auto flex justify-center">
+              <img
+                className="rounded-full h-[180px] w-[180px] my-[20px]"
+                src={require("../../Assets/Images/advisory/omansh_mathur.jpeg")}
+                alt=""
+              />
+            </div>
+            <div className="w-[400px] mr-auto ml-auto  mt-auto mb-auto">
+              <h1 className="mr-auto  ml-auto text-center">
+                <span className="text-[200%]  font-bold">Omansh Mathur</span>
+                <br />
+                <span className="yellowww text-[#063970] font-bold  text-[80%]">
+                  SDE 3 GOOGLE
+                </span>
+                <br />
+                <span className="text-[90%] font-style: italic">
+                  Graduated with a Dual Degree focused in Mathematics and
+                  Computing from Indian Institute of Technology (Banaras Hindu
+                  University), Varanasi
+                </span>
+                <br />
+              </h1>
+            </div>
+          </div>
+          <div className="flex  flex-col ">
+            <div className="w-50 h-50 rounded-full mr-auto ml-auto flex justify-center">
+              <img
+                className="rounded-full h-[180px] w-[180px] my-[20px]"
+                src={require("../../Assets/Images/advisory/pranav_vijayvaran.jpeg")}
+                alt=""
+              />
+            </div>
+            <div className="w-[400px] mr-auto ml-auto  mt-auto mb-auto">
+              <h1 className="mr-auto  ml-auto text-center">
+                <span className="text-[200%]  font-bold">
+                  Pranav Vijayvaran
+                </span>
+                <br />
+                <span className="yellowww text-[#063970] font-bold  text-[80%]">
+                  SENIOR SOFTWARE ENGINEER MICROSOFT
+                </span>
+                <br />
+                <span className="text-[90%] font-style: italic">
+                  IIT Bombay
+                </span>
+                <br />
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default OurAdvisory;
